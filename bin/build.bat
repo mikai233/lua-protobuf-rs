f959b1b6ae4b1dc0d23ea4c65b325ba7d3243f08
mikai <dreamfever2017@yahoo.com>
2025-02-22 11:10:07 +0800

    upgrade rust edition

--- bin/build.bat
+++ bin/build.bat
@@ -1,5 +1,4 @@
 cross build --target x86_64-unknown-linux-gnu --release
 cross build --target x86_64-linux-android --release
 cross build --target x86_64-pc-windows-gnu --release
-cross build --target x86_64-linux-android --release
 cross build --target aarch64-linux-android --release
